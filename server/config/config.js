dfaaec89f86f69ea0e2afdf5eca9354cff71e6b1
strngluv <strngluv@example.com>
2018-06-04 13:44:24 +0800

    Add adapt to network interface

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,19 @@
 var os = require('os');
 var env = process.env.NODE_ENV || 'development';
 
+
+
+var config = require('./config.json');
+var envConfig = config[env];
+
+Object.keys(envConfig).forEach((key) => {
+	process.env[key] = envConfig[key];
+});
+
 console.log('env******', env);
 
 if (env === 'development' || env === 'test') {
-	var config = require('./config.json');
-	var envConfig = config[env];
-
-	Object.keys(envConfig).forEach((key) => {
-		process.env[key] = envConfig[key];
-	})
-} else if (env === 'production') {
 	process.env.HOST = os.networkInterfaces().en0[1].address;
-
+} else if (env === 'production') {
+	process.env.HOST = os.networkInterfaces().wlan0[0].address;
 }
