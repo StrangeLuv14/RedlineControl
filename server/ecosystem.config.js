8b298974d93fc99dfc5560aa72e035cdd41507d6
strngluv <strngluv@example.com>
2018-08-16 20:52:20 +0800

    modify pm2 file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,16 +3,14 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-    apps: [
-
-        // First application
-        {
-            name: 'DB',
-            script: 'mongod',
-            env: {
-                COMMON_VARIABLE: 'true'
-            }
-        },
+    apps: [// First application
+        // {
+        //     name: 'DB',
+        //     script: 'mongod',
+        //     env: {
+        //         COMMON_VARIABLE: 'true'
+        //     }
+        // },
 
         // Second application
         {
